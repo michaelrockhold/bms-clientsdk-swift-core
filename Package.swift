347df35416723801c51f20a279ff3bdef314f48b
Michael Rockhold <michael@appel-rockhold.org>
2019-08-26 20:03:06 -0700

    tweak

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "../bms-clientsdk-swift-analytics-api", .branch("depodulate"))
+        .package(path: "../bms-clientsdk-swift-analytics-api")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.

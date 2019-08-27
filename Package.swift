ed5351e02d08ec95a243ecebac7a806f7810c137
Michael Rockhold <michael@appel-rockhold.org>
2019-08-26 20:54:10 -0700

    Revert "tweak"
    This reverts commit 347df35416723801c51f20a279ff3bdef314f48b.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(path: "../bms-clientsdk-swift-analytics-api")
+        .package(url: "../bms-clientsdk-swift-analytics-api", .branch("depodulate"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
